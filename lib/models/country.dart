1ba3eab1862202833c55a0896d8628feb6d9f75e
Pasha Kirichenko <p.kirichenko@in10.ru>
2022-10-26 17:39:56 +0300

    update countries

--- lib/models/country.dart
+++ lib/models/country.dart
@@ -1,23 +1,34 @@
-class Country{
+class Country {
   late String name;
   late String urlFlag;
   late String currency;
-  Country({required this.name,required this.urlFlag,required this.currency});
+  Country({required this.name, required this.urlFlag, required this.currency});
 }
 
 final List<Country> datas = [
-  Country(
-    name: 'RU', 
-    urlFlag: 'russia.png', 
-    currency: 'RUB'
-  ),
-  Country(
-    name: 'USA', 
-    urlFlag: 'russia.png', 
-    currency: 'USD'
-  ),
-  Country(
-    name: 'EU', 
-    urlFlag: 'russia.png', 
-    currency: 'EUR')
+  Country(name: 'RU', urlFlag: 'russia.png', currency: 'RUB'),
+  Country(name: 'USA', urlFlag: 'usas.png', currency: 'USD'),
+  Country(name: 'EU', urlFlag: 'es.png', currency: 'EUR'),
+  Country(name: 'JPY', urlFlag: 'japan.png', currency: 'JPY'),
+  Country(name: 'GBP', urlFlag: 'GBP.png', currency: 'GBP'),
+  Country(name: 'AUD', urlFlag: 'AUD.png', currency: 'AUD'),
+  Country(name: 'CAD', urlFlag: 'CAD.png', currency: 'CAD'),
+  Country(name: 'CHF', urlFlag: 'CHF.png', currency: 'CHF'),
+  Country(name: 'CNY', urlFlag: 'CNY.png', currency: 'CNY'),
+  Country(name: 'SEK', urlFlag: 'SEK.png', currency: 'SEK'),
+  Country(name: 'MXN', urlFlag: 'MXN.png', currency: 'MXN'),
+  Country(name: 'NZD', urlFlag: 'NDZ.png', currency: 'NZD'),
+  Country(name: 'SGD', urlFlag: 'SGD.png', currency: 'SGD'),
+  Country(name: 'HKD', urlFlag: 'HKD.png', currency: 'HKD'),
+  Country(name: 'NOK', urlFlag: 'NOK.png', currency: 'NOK'),
+  Country(name: 'KRW', urlFlag: 'KRW.png', currency: 'KRW'),
+  Country(name: 'TRY', urlFlag: 'TRY.png', currency: 'TRY'),
+  Country(name: 'INR', urlFlag: 'INR.png', currency: 'INR'),
+  Country(name: 'BRL', urlFlag: 'BRZ.png', currency: 'BRL'),
+  Country(name: 'ZAR', urlFlag: 'ZAR.png', currency: 'ZAR'),
+  Country(name: 'DKK', urlFlag: 'DKK.png', currency: 'DKK'),
+  Country(name: 'PLN', urlFlag: 'PLN.png', currency: 'PLN'),
+  Country(name: 'TWD', urlFlag: 'TWD.png', currency: 'TWD'),
+  Country(name: 'THB', urlFlag: 'THB.png', currency: 'THB'),
+  Country(name: 'MYR', urlFlag: 'MYR.png', currency: 'MYR'),
 ];
